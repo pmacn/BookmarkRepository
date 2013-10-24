bf9961df36093b40e087353f2bda80c337dc1e3c
pmacnaughton <peter@macnaughton.info>
2013-10-23 23:44:41 -0600

    Requiring https, because.. secrets! not yet requiring https on the APIs, we'll see

--- BookmarkRepository/Controllers/HomeController.cs
+++ BookmarkRepository/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace BookmarkRepository.Controllers
 {
+    [RequireHttps]
     public class HomeController : Controller
     {
         public ActionResult Index(string returnUrl)

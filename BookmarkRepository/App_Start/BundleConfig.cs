31e300b269d9fcdd9b76889ea12697fd12be2608
pmacnaughton <peter@macnaughton.info>
2013-10-24 14:28:10 -0600

    bookmark javascript bundle was bundles in the wrong order and there were dependency issues

--- BookmarkRepository/App_Start/BundleConfig.cs
+++ BookmarkRepository/App_Start/BundleConfig.cs
@@ -20,10 +20,10 @@ namespace BookmarkRepository
                 "~/Scripts/app/ajaxlogin.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bookmark").Include(
-                "~/Scripts/app/bookmark.bindings.js",
                 "~/Scripts/app/bookmark.datacontext.js",
                 "~/Scripts/app/bookmark.model.js",
-                "~/Scripts/app/bookmark.viewmodel.js"));
+                "~/Scripts/app/bookmark.viewmodel.js",
+                "~/Scripts/app/bookmark.ui.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
